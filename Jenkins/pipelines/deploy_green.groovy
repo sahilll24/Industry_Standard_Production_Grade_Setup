f82266edcea643299a44a61c13b394f3a7fc5ebc
sahilll24 <sahilsaykar2407@gmail.com>
2026-01-23 15:06:26 +0530

    terraform ansible connection issues

--- Jenkins/pipelines/deploy_green.groovy
+++ Jenkins/pipelines/deploy_green.groovy
@@ -6,19 +6,18 @@ stage("Deploy ${env.NEW_COLOR}") {
         """
     }
 
-    dir("ansible") {
-sh """
+dir("ansible") {
+sh '''
 #!/bin/bash
 set -e
 
-export AWS_REGION=ap-south-1
-export NEW_COLOR=${NEW_COLOR}
+echo "NEW_COLOR is $NEW_COLOR"
 
 echo "⏳ Waiting for EC2 instances to be registered in SSM..."
 
 for i in {1..30}; do
   COUNT=$(aws ssm describe-instance-information \
-    --region $AWS_REGION \
+    --region ap-south-1 \
     --query "InstanceInformationList[?PingStatus=='Online'] | length(@)" \
     --output text)
 
@@ -31,12 +30,10 @@ for i in {1..30}; do
   sleep 20
 done
 
-echo "📦 Inventory after SSM ready:"
 ansible-inventory -i inventory/aws_ec2.yml --graph
-
-echo "🚀 Running Ansible deployment"
 ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
-"""
+'''
 }
+
 
 }
