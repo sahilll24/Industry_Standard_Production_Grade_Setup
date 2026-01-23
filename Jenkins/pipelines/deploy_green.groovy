e8f29100080bf61284fa84b17383d15f1b1b8610
sahilll24 <sahilsaykar2407@gmail.com>
2026-01-23 11:48:33 +0530

    terraform ansible connection issues

--- Jenkins/pipelines/deploy_green.groovy
+++ Jenkins/pipelines/deploy_green.groovy
@@ -7,25 +7,27 @@ stage("Deploy ${env.NEW_COLOR}") {
     }
 
     dir("ansible") {
-        sh '''
-#!/bin/bash
-set -e
-export NEW_COLOR=${NEW_COLOR}
+        sh """
+        set -e
+        export NEW_COLOR=${env.NEW_COLOR}
 
-echo "⏳ Waiting for EC2 instances to appear in Ansible inventory..."
+        echo "⏳ Waiting for EC2 instances to appear in Ansible inventory..."
 
-for i in {1..20}; do
-  COUNT=$(ansible-inventory -i inventory/aws_ec2.yml --list | grep instance_id | wc -l)
-  if [ "$COUNT" -gt 0 ]; then
-    echo "✅ Inventory ready"
-    break
-  fi
-  echo "Waiting for instances (attempt $i)..."
-  sleep 15
-done
+        for i in {1..20}; do
+          COUNT=\$(ansible-inventory -i inventory/aws_ec2.yml --list | grep instance_id | wc -l)
+          if [ "\$COUNT" -gt 0 ]; then
+            echo "✅ Inventory ready"
+            break
+          fi
+          echo "Waiting for instances (attempt \$i)..."
+          sleep 15
+        done
 
-ansible-inventory -i inventory/aws_ec2.yml --graph
-ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
-'''
+        echo "📦 Final inventory:"
+        ansible-inventory -i inventory/aws_ec2.yml --graph
+
+        echo "🚀 Running Ansible deployment"
+        ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
+        """
     }
 }
--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -37,7 +37,7 @@ module "asg" {
 
   project_name          = "doctor-app"
   env                   = "dev"
-  ami                   = "ami-02b8269d5e85954ef"
+  ami                   = "ami-0ced6a024bb18ff2e"
   instance_type         = "t3.micro"
   subnets               = module.vpc.public_subnets
   app_sg                = module.sg.ec2_sg
