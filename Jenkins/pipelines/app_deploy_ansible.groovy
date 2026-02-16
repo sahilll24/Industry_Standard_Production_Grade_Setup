0baf8a8db378d6ca4d4dd452358fe01475908882
sahilll24 <sahilsaykar2407@gmail.com>
2026-02-16 17:43:51 +0530

    Ansible Fixes

--- Jenkins/pipelines/app_deploy_ansible.groovy
+++ Jenkins/pipelines/app_deploy_ansible.groovy
@@ -1,8 +1,13 @@
 stage("Ansible Deploy (${env.NEW_COLOR})") {
     dir("ansible") {
         sh """
-          ansible-inventory -i inventory/aws_ec2.yml --graph
-          ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
+              export AWS_REGION=${AWS_REGION}
+
+              ansible-galaxy collection install amazon.aws --force
+              ansible-galaxy collection install community.aws --force
+
+              ansible-inventory -i inventory/aws_ec2.yml --graph
+              ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
         """
     }
 }
