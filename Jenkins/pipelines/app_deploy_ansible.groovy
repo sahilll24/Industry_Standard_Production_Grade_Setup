637d78bc7a5b236388bda8c47a8a8e689427c21f
sahilll24 <sahilsaykar2407@gmail.com>
2026-02-17 15:05:21 +0530

    Ansible python interpreter issue fixed

--- Jenkins/pipelines/app_deploy_ansible.groovy
+++ Jenkins/pipelines/app_deploy_ansible.groovy
@@ -7,8 +7,7 @@ stage("Ansible Deploy (${env.NEW_COLOR})") {
               ansible-galaxy collection install community.aws --force
 
               ansible-inventory -i inventory/aws_ec2.yml --graph
-              ansible-playbook -i inventory/aws_ec2.yml playbook/deploy.yml
-              ansible-playbook -vvv output
+              ansible-playbook -vvv -i inventory/aws_ec2.yml playbook/deploy.yml
         """
     }
 }
